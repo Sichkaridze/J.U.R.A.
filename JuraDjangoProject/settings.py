563eb75637076f3d6dd4aa2ee1663252cd17b655
Dmytro <dima.sichkar2003@gmail.com>
2025-02-02 20:17:07 +0200

    changed Timezone to Europe/Kiev and defined custom user model in project settings

--- JuraDjangoProject/settings.py
+++ JuraDjangoProject/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My apps:
-    'task_manager'
+    'task_manager',
+
+    # Third-party apps:
+
 ]
 
 MIDDLEWARE = [
@@ -110,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
@@ -126,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "task_manager.Worker"
