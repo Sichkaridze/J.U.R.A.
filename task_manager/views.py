778fc259520fe551192475201bd5f2be692f5a41
Dmytro <dima.sichkar2003@gmail.com>
2025-02-05 15:16:47 +0200

    Create urls and views for task_manager app, make migrations

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from task_manager.models import Worker, Project, Task
 
-# Create your views here.
+
+def home_view(request):
+    """
+    View для відображення головної сторінки.
+    """
+    return render(request, 'task_manager/test.html')
+
+
+class TaskListView(ListView):
+    model = Task
+
+
+class TaskDetailView(DetailView):
+    model = Task
+
+
+
+
+class ProjectListView(ListView):
+    model = Project
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = '__all__'
+    success_url = reverse_lazy("task-manager:project-list")
+
+
+class ProjectUpdateView(UpdateView):
+    model = Project
+    fields = "__all__"
+    success_url = reverse_lazy("task-manager:project-list")
+
+
+class ManufacturerDeleteView(DeleteView):
+    model = Project
+    success_url = reverse_lazy("task-manager:project-list")
