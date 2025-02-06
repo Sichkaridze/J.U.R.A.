8fac198fee8f5ebe7e467a9bf1a637bea5116e45
Dmytro <dima.sichkar2003@gmail.com>
2025-02-06 23:17:23 +0200

    minor corrections

--- JuraDjangoProject/settings/base.py
+++ JuraDjangoProject/settings/base.py
@@ -106,11 +106,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -118,11 +113,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static/"),
-]
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "static/"
 STATIC_ROOT = "staticfiles/"
+
+STATICFILES_DIRS = [BASE_DIR / "static",]
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
